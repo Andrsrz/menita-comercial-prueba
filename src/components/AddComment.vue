b08565554ecb3e0c33704fec082655a082fde319
Andres Ruiz <andrsruiz@gmail.com>
2021-01-19 16:45:30 -0600

    Emit event when adding comment

--- src/components/AddComment.vue
+++ src/components/AddComment.vue
@@ -1,14 +1,33 @@
 <template>
 	<div class='new-comment'>
 		<p>Add a Comment</p>
-		<textarea name="" id="" rows="10" cols="" tabindex=""></textarea>
-		<button>Send</button>
+		<textarea rows="10" v-model='comment'></textarea>
+		<button @click='sendComment'>Send</button>
 	</div>
 </template>
 
 <script>
+import { EventBus } from '@/eventBus.js'
+
 export default {
-	name: 'AddComment'
+	name: 'AddComment',
+	data(){
+		return{
+			comment: ''
+		}
+	},
+	methods: {
+		sendComment(){
+			EventBus.$emit('send-comment', {
+					body: this.comment,
+					email: 'fake@fake.fake',
+					id: 1000,
+					name: 'fakeuser',
+					postId: 0
+			})
+			this.comment = ''
+		}
+	}
 }
 </script>
 
