f39f7aaf8fecf5eca49b3794c4189befc36d4e00
Andres Ruiz <andrsruiz@gmail.com>
2021-01-19 16:19:09 -0600

    Create AddComment component

---
+++ src/components/AddComment.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class='new-comment'>
+		<p>Add a Comment</p>
+		<textarea name="" id="" rows="10" cols="" tabindex=""></textarea>
+		<button>Send</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'AddComment'
+}
+</script>
+
+<style scoped>
+textarea {
+	width: 100%;
+	resize: vertical;
+}
+</style>
